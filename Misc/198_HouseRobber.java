020fc28a6ae7c05050451580f40c765f6a6b9b65
Tom <thomashollis1@gmail.com>
2018-08-18 15:31:46 -0400

    v1.0

---
+++ Misc/198_HouseRobber.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int rob(int[] nums) {
+        int even_total = 0;
+        int odd_total = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                even_total += nums[i];
+                if (even_total>odd_total) {
+                    even_total = even_total;
+                }
+                else {
+                     even_total = odd_total;
+                }
+            } 
+            else {
+                odd_total += nums[i];
+                if (even_total>odd_total) {
+                    odd_total = even_total;
+                }
+                else {
+                     odd_total = odd_total;
+                }
+            }
+        }
+
+        if (even_total>odd_total) {
+            return even_total;
+        }
+        else {
+             return odd_total;
+        }
+    }
+}
