fd150497dd4e1e28595b896a481a25a5ae36ab68
Tom <thomashollis1@gmail.com>
2018-09-04 09:02:18 -0400

    v2.1

--- Misc/7_ReverseInteger.java
+++ Misc/7_ReverseInteger.java
@@ -4,14 +4,12 @@ class Solution {
 
         while (x != 0) {
             int digit = x_reverse*10 + x%10;
-
             if ((digit - x%10)/10 != x_reverse) {
                 return 0;
             } 
             else {
                 x_reverse = digit;
             }
-            
             x = x/10;   
         }
         return x_reverse;
