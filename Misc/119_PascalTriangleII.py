1fd800c6dd853ccc6ab82516d442cd5ca76c3574
Tom <thomashollis1@gmail.com>
2018-09-14 02:17:17 -0400

    v1.1

---
+++ Misc/119_PascalTriangleII.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        pascal = []
+        pascal_row = [None]*(rowIndex+1)
+        
+        for i in range(rowIndex+1):
+            pascal.append([1]*(i+1))
+            for j in range(1,i):
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+        
+        for k in range(rowIndex+1):
+            pascal_row[k] = pascal[rowIndex][k] 
+        
+        return pascal_row
