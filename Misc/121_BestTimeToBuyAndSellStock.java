fe848077f30094afb807d504b48df878bdef1b58
Tom <thomashollis1@gmail.com>
2018-09-30 12:42:26 -0400

    v2.1

--- Misc/121_BestTimeToBuyAndSellStock.java
+++ Misc/121_BestTimeToBuyAndSellStock.java
@@ -3,15 +3,12 @@ class Solution {
         if (prices == null || prices.length <= 1) {
             return 0;
         }
-
         int min = prices[0]; 
         int profit = 0;
-
         for(int i = 1; i<prices.length; i++){
             profit = Math.max(profit, prices[i]-min);
             min = Math.min(min, prices[i]);
         }
-
         return profit;
     }
 }
