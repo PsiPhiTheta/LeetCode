24fdddbcce99e48cb13310b7bc5827e905fe414d
Tom <thomashollis1@gmail.com>
2018-12-01 00:13:04 -0500

    v2.1

---
+++ Misc/371_SumOfTwoIntegersWithoutUsingSymbols.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        lst = [] 
+        
+        lst.append(a)
+        lst.append(b)
+        
+        return sum(lst)
