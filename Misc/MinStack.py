29924f1cb4bde290d409473ad47c516e531d998d
Tom <thomashollis1@gmail.com>
2018-12-27 09:32:42 +0100

    v2.1

---
+++ Misc/MinStack.py
@@ -0,0 +1,44 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.mystack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.mystack.append(x)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.mystack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        n = len(self.mystack)
+        return self.mystack[n-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return min(self.mystack)
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
