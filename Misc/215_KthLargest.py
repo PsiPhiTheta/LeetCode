9555d29ba20c07b5481180a6d522c30ef3c300dc
Tom <thomashollis1@gmail.com>
2019-01-15 23:30:30 -0500

    v2.1

---
+++ Misc/215_KthLargest.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
+        nums.sort() 
+        
+        return nums[-k]
+        
